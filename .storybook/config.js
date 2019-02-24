42a199ae493d3af54c6eebf974b58ab1cc023e5d
Chang Wang <garnwraly@gmail.com>
2019-02-24 16:18:03 -0500

    fix storybook heights

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,13 @@
 import { configure } from '@storybook/react';
+import { injectGlobal } from 'emotion';
 
 const req = require.context('../src', true, /\.stories\.(js|ts|tsx)$/);
+
+injectGlobal`
+  html, body, #root {
+    height: 100%;
+  } 
+`;
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
