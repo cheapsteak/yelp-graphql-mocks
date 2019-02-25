6276078011920dfbd2faee687a1cb62800d00e9d
Chang Wang <garnwraly@gmail.com>
2019-02-24 19:27:50 -0500

    Use automocks in ResultsMap story

--- src/ResultsMap/ResultsMap.stories.tsx
+++ src/ResultsMap/ResultsMap.stories.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { css } from 'emotion';
 import { MockedProvider } from 'react-apollo/test-utils';
+import faker from 'faker';
 
 import ResultsMap from './ResultsMap';
 import * as GraphQLTypes from '../graphqlTypes';
 import InteractionStateContainer from '../InteractionStateContainer';
 import { markerInfoQuery } from './Info';
+import AutoMockedProvider from '../utils/mocks/AutoMockedProvider';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -89,6 +91,38 @@ storiesOf('SearchPage', module)
     </div>
   ))
   .add('ResultsMap', () => {
+    return (
+      <InteractionStateContainer.Provider>
+        <ResultsMap businesses={businesses} />
+      </InteractionStateContainer.Provider>
+    );
+  });
+
+storiesOf('SearchPage', module)
+  .addDecorator(story => (
+    <div
+      className={css`
+        width: 100%;
+        height: 100%;
+      `}
+    >
+      <AutoMockedProvider
+        mockResolvers={{
+          Business: (root, { id }) => ({
+            id: () => id,
+            name: () => faker.company.companyName(),
+            location: () => ({
+              address1: () => faker.address.streetAddress(),
+              address2: () => faker.address.secondaryAddress(),
+            }),
+          }),
+        }}
+      >
+        {story()}
+      </AutoMockedProvider>
+    </div>
+  ))
+  .add('ResultsMap (Auto-Mocked)', () => {
     return (
       <InteractionStateContainer.Provider>
         <ResultsMap businesses={businesses} />
